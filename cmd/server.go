c373eb770b10d69cd9e0237501ba890f1e2d8c79
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-11-03 13:47:49 +0530

    Implemented simple I/O. fix require in file paths

--- cmd/server.go
+++ cmd/server.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Ankit-1204/FlintDB.git/internals"
 )
 
-func start() error {
-	internals.Start()
-	for {
-		select {}
+func main() {
+	fmt.Println("DBname: ")
+	var dbname string
+	fmt.Scan(&dbname)
+	db, err := internals.Open(dbname)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	for {
+		fmt.Println("give operation: ")
+		var op string
+		fmt.Scan(&op)
+		if op == "G" {
+			fmt.Println("give key and value: ")
+			var (
+				key string
+			)
+			fmt.Scan(&key)
+			v := db.Get(key)
+			fmt.Print(v)
+		} else if op == "P" {
+			fmt.Println("give key and value: ")
+			var (
+				key   string
+				value string
+			)
+			fmt.Scan(&key, value)
+			valu := []byte(value)
+			err := db.Put(key, valu)
+			if err != nil {
+				fmt.Print(err)
+				return
+			}
+		}
+	}
+
 }
